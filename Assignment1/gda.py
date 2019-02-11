ce1fe6d2d95fa40bf20d3156245ccebb34c92265
Ankit Solanki <ankit03june@gmail.com>
2019-02-12 02:48:53 +0530

    GDA files not complete

--- Assignment1/gda.py
+++ Assignment1/gda.py
@@ -15,6 +15,7 @@ class Datafiles:
         self.process()
 
     def process(self):
+
         # Read from CSV file
         csvfileX=open(self.fileX, 'r')
         csvfileY=open(self.fileY, 'r')
@@ -22,13 +23,19 @@ class Datafiles:
         X = list(csv.reader(csvfileX))
         Y = list(csv.reader(csvfileY))
 
+        # Alaska - 1 || Canada - 0
         # Convert list format to float format
         samples = len(X)
         X1 = []
         Y1 = []
         for i in range(samples):
+            X[i] = X[i][0].split("  ")
             X1.append([float(X[i][0]), float(X[i][1])])
-            Y1.append(float(Y[i][0]))
+            
+            if Y[i][0].lower() == "alaska":
+                Y1.append(0)
+            else:
+                Y1.append(1)
 
         X = X1
         Ylist = Y1
@@ -54,23 +61,47 @@ class Datafiles:
             for j in range(len(meanX)):
                 X[i][j] = (X[i][j] - meanX[j])/varianceX[j]
 
-        print(meanX)
-        print(varianceX)
+        # Data Normalized
 
+        # Parition Data and Calculate Mean and Variance
         partition = {1: {'x': [], 'y': []}, 0: {'x':[], 'y':[]}}
+        mu0 = np.array([0., 0.])
+        mu1 = np.array([0., 0.])
+
+        phi = 0
         for i in range(samples):
             if Ylist[i]:
                 partition[1]['x'].append(float(X[i][0]))
                 partition[1]['y'].append(float(X[i][1]))
+                mu1 += X[i]
+                phi += 1
             else:
                 partition[0]['x'].append(float(X[i][0]))
                 partition[0]['y'].append(float(X[i][1]))
+                mu0 += X[i]
 
+        phi /= samples
+        mu0 = mu0 / len(partition[0]['x'])
+        mu1 = mu1 / len(partition[1]['x'])
 
+        Xlist = X
         X = np.array(X)
-        # Insert the X0 term (all ones: constant)
-        X = np.insert(X, 0, 1.0, axis=1)
         Y = np.array(Ylist)
+
+        print("[*] Mean of distribution of X (given y=0): {} and (given y=1): {}".format(mu0, mu1))
+        print("[*] Parameter of distribution of y: {}".format(phi))
+
+        sigma = np.zeros((X.shape[1], X.shape[1]))
+        for i in range(samples):
+            if Y[i]:
+                sigma += np.outer(X[i] - mu1, X[i] - mu1)
+            else:
+                sigma += np.outer(X[i] - mu0, X[i] - mu0)
+
+        sigma /= samples
+        sigma_inverse = np.linalg.inv(sigma)
+
+        print("[*] Covariance Matrix: {}".format(sigma))
 
         self.X = X
         self.Y = Y 
@@ -78,65 +109,4 @@ class Datafiles:
         self.samples = samples
         self.features = len(meanX)
 
-data = Datafiles("./data/logisticX.csv", "./data/logisticY.csv")
-
-def sigmoid(z):
-    return 1/(1 + np.exp(-z))
-
-def sigmoid_derivative(z):
-    return sigmoid(z)*(1-sigmoid(z))
-
-
-threshold = 0.00001
-_iter = 0
-theta = np.array([0, 0, 0])
-
-while True:
-    htheta = np.matmul(data.X, theta)
-    gtheta = sigmoid(htheta)
-    gradient = np.dot(data.X.T, (data.Y - gtheta))
-
-    # Calculate Hessian
-    H = np.zeros((data.features+1, data.features+1))
-    for i in range(data.samples):
-        H = H - (sigmoid_derivative(htheta[i]))*(np.outer(data.X[i].T, data.X[i]))
-    _iter += 1
-
-    theta_old = theta
-    # Update theta
-
-    theta = theta - np.matmul(np.linalg.inv(H), gradient)
-
-    # Check if converged
-    if(np.linalg.norm(theta - theta_old) < threshold):
-        break;
-
-print("[*] Number of iterations {i}".format(i=_iter))
-print(theta)
-
-
-def plot_theta(data, theta):
-    fig = plt.figure(figsize=(12, 10))
-    foot = fig.add_subplot(1,1,1)
-
-    foot.scatter(data.partition[0]['x'], data.partition[0]['y'], label="y=0 | Training Data", color= "red", marker= ".")
-    foot.scatter(data.partition[1]['x'], data.partition[1]['y'], label="y=1 | Training Data", color= "green", marker= "+")
-
-    x=np.linspace(min(min(data.partition[0]['x']), min(data.partition[1]['x'])), max(max(data.partition[0]['x']), max(data.partition[0]['x'])), data.samples)
-    y = -1 * (theta[1]*x + theta[0])/theta[2]
-
-    foot.plot(x, y, label="Decision boundary", color ='blue')
-
-    plt.xlabel('x - axis')
-    plt.ylabel('y - axis')
-    # plt.ylim(bottom=0)
-
-    plt.legend()
-    plt.title('Q3(a) - Logistic Regression: Using Newton\'s Method')
-
-    plt.savefig('./graphs/logistic_regression_newton_method.png')
-
-
-plot_theta(data, theta)
-
-
+data = Datafiles("./data/q4x.dat", "./data/q4y.dat")
