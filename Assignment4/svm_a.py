3687230e4ae0387bb500eb11cbb1c4b6910cf8d5
Ankit Solanki <ankit03june@gmail.com>
2019-04-22 19:30:50 +0530

    SVM-added | code running

--- Assignment4/svm_a.py
+++ Assignment4/svm_a.py
@@ -3,6 +3,7 @@
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 import numpy as np
+from sklearn.metrics import f1_score
 
 class ModelSVM:
     def __init__(self, kernel="linear"):
@@ -15,21 +16,24 @@ class ModelSVM:
     def fit(self, p_object, verbose=False):
         # model = GridSearchCV(SVC(kernel=self.kernel, class_weight="balanced", gamma="scale"),
         #                      self.params_grid, n_jobs=-1, cv=5,)
+        print("[*] Training")
         model = SVC(kernel=self.kernel, class_weight="balanced", gamma="scale")
 
-        i = 0
+        # i = -1
         for X, Y in p_object.get_train_data_by_episode():
-            print(i, end=" ")
+            # print(i, end=" ")
+            # print(Y.ravel())
             model.fit(X, Y.ravel())
-            i+=1
+            # i+=1
 
-        if verbose:
-            print("[*] Best Estimators from Grid Search: {}".format(
-                model.best_estimator_))
+        # if verbose:
+        #     print("[*] Best Estimators from Grid Search: {}".format(
+        #         model.best_estimator_))
 
         self.model = model
 
     def score(self, p_object):
+        print("[*] Score:")
         first=True
         for X, Y in p_object.get_test_data_by_episode():
             if first:
@@ -40,5 +44,10 @@ class ModelSVM:
             _X = np.concatenate((_X, X), axis=0)
             _Y = np.concatenate((_Y, Y), axis=0)
 
-        accuracy = self.model.score(_X, _Y.ravel())
-        return accuracy
+        y_pred = self.model.predict(_X)
+        fscore = f1_score(y_pred, _Y.ravel())
+
+        # print(y_pred)
+        # print(_Y.ravel())
+        print(fscore)
+        return fscore
